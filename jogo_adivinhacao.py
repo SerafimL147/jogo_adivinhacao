c2f3a04adb4a33b6b2986b37f1526731b052ad85
Marcus Serafim <marcusserafimtks@gmail.com>
2022-12-28 15:21:26 -0300

    Update jogo_adivinhacao.py

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -5,23 +5,24 @@ def jogo():
 
     numero = random.randint(0, 100)
 
-    tentativas = 10
+    tentativas = 11
     print(numero)
-    while tentativas > 0:
+    while tentativas > 1:
 
         tentativas = tentativas -1
 
-        print('Bem vindo, você tem 10 tentativas para adivinhar o número')
+        print(f'Bem vindo, você tem {tentativas} tentativas para adivinhar o número')
         n_usuario = int(input('Adivinhe o número sorteado: '))
 
         if n_usuario > numero:
-            print(f'Seu número é maior que o número sorteado, tente novamente! Restam {tentativas} tentativas')
+            print('Seu número é maior que o número sorteado, tente novamente!')
 
         elif n_usuario < numero:
-            print(f'Seu número é menor que o número sorteado, tente novamente! Restam {tentativas} tentativas')
+            print('Seu número é menor que o número sorteado, tente novamente!')
 
         elif n_usuario == numero:
             print('Parabéns você acertou o número!')
+            exit(jogo())
 
 
 
